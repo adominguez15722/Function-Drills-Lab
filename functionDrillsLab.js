c86a1b4517f1ff02058a3ef1f7cf85c55b812593
Anthony <adominguez15722@gmail.com>
2022-01-10 23:26:58 -0600

    Problem 15 is solved

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -289,9 +289,9 @@ let namesArr = ['Cameron', 'Riley', 'Eric', 'Brenna', 'Karl']
 
 //CODE HERE
 
-const bestMovie = (title) => {return `${title} is the best movie ever!`};
+// const bestMovie = (title) => {return `${title} is the best movie ever!`};
 
-console.log(bestMovie("Sharknado"));
+// console.log(bestMovie("Sharknado"));
 
 
 ////////////////// PROBLEM 15 ////////////////////
@@ -308,6 +308,22 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 //CODE HERE
 
 
+
+function bigOrSmall(arr){
+  let answers = []
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 100){
+      answers.push('big')
+    } else{
+      answers.push('small')
+    };
+  };
+  return answers
+  
+};
+let arrayEvaluator = bigOrSmall(bigOrSmallArray);
+console.log(arrayEvaluator);
+
 ////////////////// PROBLEM 16 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
@@ -317,7 +333,9 @@ let loser = 'Glimmer'
 */
 
 //CODE HERE
-
+// function theEliminator(name1, name2){
+//   for(let i = 0; i < contestants.length; i++)
+// }
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
